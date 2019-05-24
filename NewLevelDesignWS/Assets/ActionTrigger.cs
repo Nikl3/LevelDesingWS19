c90a332d8f7a86980d2eab92b2e6981ad7005a4a
Nikl3 <niko.tuuli@hotmail.com>
2019-05-24 13:38:42 +0300

    pil triggering and interactions

---
+++ NewLevelDesignWS/Assets/ActionTrigger.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionTrigger : MonoBehaviour {
+
+    public Text interactionText;
+    List<string> notifyTexts = new List<string>();
+    public float notifyTime;
+    float notifyTimer;
+
+    private void Start() {
+        interactionText.text = "";
+        notifyTimer = notifyTime;
+    }
+
+    public void Update() {
+        if (notifyTexts.Count > 0) {
+            interactionText.text = notifyTexts[0];
+            notifyTimer -= Time.deltaTime;
+            if (notifyTimer < 0) {
+                notifyTexts.RemoveAt(0);
+                if (notifyTexts.Count == 0) {
+                    ResetText();
+                }
+                notifyTimer = notifyTime;
+            }
+        }
+    }
+
+        private void OnTriggerEnter(Collider other) {
+        var interactable = other.GetComponent<Interactable>();
+        if (interactable) {
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        var interactable = other.GetComponent<Interactable>();
+        if (interactable) {
+            if (notifyTexts.Count == 0) {
+                interactionText.text = interactable.description;
+            }
+
+            if (Input.GetKeyDown(KeyCode.F)) {
+                interactable.action.Invoke();
+
+            }
+        }
+
+    } 
+
+    private void OnTriggerExit(Collider other) {
+        if (notifyTexts.Count == 0) {
+            ResetText();
+        }
+    }
+
+    public void ResetText() {
+        interactionText.text = "";
+    }
+    public void NotifyText(string s) {
+        notifyTexts.Add(s);
+
+    }
+}
