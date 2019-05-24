ced8a282ce9bb409202f420192235c86b08b991a
LAPTOP-3BTFH1M6\Omistaja <jaani.kaapa@gmail.com>
2019-05-24 13:47:39 +0300

    Tilpehööri
    Taidehärdelli

---
+++ NewLevelDesignWS/Assets/Scripts/PilTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PilTrigger : MonoBehaviour {
+
+
+    public void EatPill() {
+        print("pill eaten");
+        Destroy(gameObject);
+
+    }
+}
